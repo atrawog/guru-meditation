349f0670f84cf3e1638aeee7f5f8d12d8d2bd57d
atrawog <atrawog@gmail.com>
2025-01-16 14:37:13 +0000

    Add loop to wait for jupyterhub to start after launching supervisord

--- config/postcreate.sh
+++ config/postcreate.sh
@@ -13,5 +13,19 @@ set +a
 
 /usr/local/bin/fixpermission.sh
 /usr/local/bin/setpassword.sh
+echo "Starting supervisord"
 sudo /usr/sbin/supervisord -c /etc/supervisord.conf > /dev/null 2>&1 &
+while true; do
+    # Get the status of jupyterhub
+    status=$(supervisorctl status jupyterhub | awk '{print $2}')
+    
+    # Check if the status is RUNNING
+    if [ "$status" == "RUNNING" ]; then
+        echo "jupyterhub is running."
+        break
+    else
+        echo "Waiting for jupyterhub to start..."
+        sleep 1
+    fi
+done
 
